7bc043b4aaaff3f31be3d53e0c1f3cf0a673c512
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-18 07:11:54 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function appendCat(Broom) {
+    return [...cats, "Broom"];
+}
+function prependCat(Arnold) {
+    return ["Arnold", ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
